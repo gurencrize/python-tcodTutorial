73c18fab75d701b6b061dc94387861ce9c943b1b
crize <34015092+gurencrize@users.noreply.github.com>
2022-02-21 23:04:27 +0900

    とりあえず残しとく

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
+from entity import Entity
 from input_handlers import EventHandler
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
-
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
 
     # TCODで使用するフォントの読み込み？
     tileset = tcod.tileset.load_tilesheet(
@@ -17,6 +15,12 @@ def main() -> None:
     )
 
     event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
@@ -28,26 +32,9 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         # ゲーム根幹のループ
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
-            # 画面更新
-            context.present(root_console)
-
-            # 描画されている物を消去する
-            root_console.clear()
-
-            # イベントを受け付ける箇所
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-                
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            engine.render(console=root_console, context=context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
 
 
 if __name__ == "__main__":
